1dec16f2f3c1b75fba3c2a9e59cc6608ff827bc2
Teelaw <l.adeotoun99@gmail.com>
2022-03-11 02:08:32 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>i
+/**
+ * main - print alphabet
+ * Description: display alphabet
+ * Return: 0 if the programming ran to completion
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
