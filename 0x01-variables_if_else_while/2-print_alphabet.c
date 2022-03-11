08777ba7b0ebf2c2a0b0f491685c4fe3b8203ba3
Teelaw <l.adeotoun99@gmail.com>
2022-03-11 03:44:39 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,12 @@
-#include <stdio.h>i
+#include <stdio.h>
 /**
  * main - print alphabet
+ *
  * Description: display alphabet
  * Return: 0 if the programming ran to completion
  */
 int main(void)
+
 {
 	char i;
 
