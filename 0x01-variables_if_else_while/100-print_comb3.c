5f5be54aab8fca5317618247d36b11e4abccc059
Teelaw <l.adeotoun99@gmail.com>
2022-03-11 06:13:36 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ *
+ * Description: display alphabet
+ * Return: 0 if the programming ran to completion
+ */
+
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 9; i++)
+for (j = i + 1; j <= 9; j++)
+{
+	putchar(i + '0');
+	putchar(j + '0');
+	if (i < 8)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+}
+putchar('\n');
+return (0);
+}
