6ce2b3f6c0f79269b385e02ad5e4428e2e7e933a
Teelaw <l.adeotoun99@gmail.com>
2022-03-11 04:44:53 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ *
+ * Description: display alphabet
+ * Return: 0 if the programming ran to completion
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+{
+	putchar(i);
+	if (i != '9')
+		{
+		putchar(' ');
+		putchar(' ');
+		}
+}
+putchar('\n');
+return (0);
+}
