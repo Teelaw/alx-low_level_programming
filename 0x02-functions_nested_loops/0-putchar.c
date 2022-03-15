e3f34fa5d6fd0f518901c14a79641a5884db0cb3
Teelaw <l.adeotoun99@gmail.com>
2022-03-15 07:00:16 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: program starts and end here
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char text[9] = "main";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+		_putchar('\n');
+		return (0);
+}
