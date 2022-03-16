bd194e7466ae62825090901642f3b601baaa2f03
Teelaw <l.adeotoun99@gmail.com>
2022-03-15 18:01:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _putchar(char c);
+int _putchar(char c)
 {
-char text[9] = "main.h";
-int i;
-for (i = 0; i < 9; i++)
-{
-_putchar (text[i]);
-}
-_putchar('\n');
-return (0);
+	return (write(1, &c, 1));
 }
